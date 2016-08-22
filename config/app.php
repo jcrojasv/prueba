262953ec7aa2836bea50ba43a3e177df7f6efbdf
jcrojasv <jcrojasv@gmail.com>
2016-08-22 16:15:59 -0500

    Instalacion de la plantilla AdminLte

--- config/app.php
+++ config/app.php
@@ -163,6 +163,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Otros Services Providers
+         */
+        Acacha\AdminLTETemplateLaravel\Providers\AdminLTETemplateServiceProvider::class,
     ],
 
     /*
@@ -208,6 +212,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'AdminLTE' => Acacha\AdminLTETemplateLaravel\Facades\AdminLTE::class,
 
     ],
 
